efd71187ef58c68170f6ae12e9b8f43c08241b83
whwnsrb <whwnsrb2442@naver.com>
2022-01-20 18:43:05 +0900

    0120

--- src/main/java/kr/co/replydto/replydto/replyDTO/ReplyDTO.java
+++ src/main/java/kr/co/replydto/replydto/replyDTO/ReplyDTO.java
@@ -1,17 +1,23 @@
 package kr.co.replydto.replydto.replyDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class ReplyDTO {
     private Long replyId;
     private Long boardId;
     private String membernickname;
     private String replycontent;
-    private Date regdate;
-    private Date updatedate;
+    private LocalDateTime regdate;
+    private LocalDateTime updatedate;
     private String memberId;
     private String secretreply;
 
